03ca6af3db976060ec72bdd7c6c21c787a89bd34
Sidak <sidak.pasrichassp@gmail.com>
2020-07-29 23:16:46 +0530

    Consuming data from RickAndMorty API using Jackson

--- src/main/java/com/udacity/bootstrap/BootstrapApplication.java
+++ src/main/java/com/udacity/bootstrap/BootstrapApplication.java
@@ -1,13 +1,31 @@
 package com.udacity.bootstrap;
 
+import com.udacity.bootstrap.entity.ShowCharacter;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class BootstrapApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootstrapApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder){
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception{
+		return args -> {
+			ShowCharacter character = restTemplate.getForObject("https://rickandmortyapi.com/api/character/1", ShowCharacter.class);
+			System.out.println(character.toString());
+		};
 	}
 
 }
---
+++ src/main/java/com/udacity/bootstrap/entity/ShowCharacter.java
@@ -0,0 +1,82 @@
+package com.udacity.bootstrap.entity;
+
+public class ShowCharacter {
+    private int id;
+    private String name;
+    private String status;
+    private String species;
+    private String type;
+    private String gender;
+
+    public ShowCharacter() {
+    }
+
+    public ShowCharacter(int id, String name, String status, String species, String type, String gender) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+        this.species = species;
+        this.type = type;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "ShowCharacter{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", status='" + status + '\'' +
+                ", species='" + species + '\'' +
+                ", type='" + type + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+}
